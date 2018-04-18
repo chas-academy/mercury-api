9a1f71d305343c2c6f4cc109e9bcc1916513657d
ulander-a <alve.ulander@chasacademy.se>
2018-04-18 15:43:55 +0200

    add ESLint and dependencies, fix autofixable errors/warnings

--- src/controllers/Sessions.js
+++ src/controllers/Sessions.js
@@ -1,25 +1,25 @@
-import * as Sessions from '../lib/Sessions'
+import * as Sessions from '../lib/Sessions';
 
 export default {
   authenticate(req, res) {
     Sessions
       .auth(req, res)
-      .then(auth => {
-        res.status(auth.status).send(auth.data)
+      .then((auth) => {
+        res.status(auth.status).send(auth.data);
       })
-      .catch(error => {
-        res.status(400).send(error)
-      })
+      .catch((error) => {
+        res.status(400).send(error);
+      });
   },
 
   signOut(req, res) {
     Sessions.signOut(req)
       .then(() => {
-        res.status(200).send()
-      })
+        res.status(200).send();
+      });
   },
 
   verifyToken(req, res) {
-    res.status(200).send()
-  }
-}
+    res.status(200).send();
+  },
+};
--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -1,4 +1,4 @@
-import * as Users from '../lib/Users'
+import * as Users from '../lib/Users';
 
 export default {
   list(req, res) {
@@ -8,19 +8,19 @@ export default {
           res,
           query: req.query,
           returnData: true,
-          jsonData: true
+          jsonData: true,
         }),
-        Users.pages({ query: req.query })
+        Users.pages({ query: req.query }),
       ])
-      .then(promises => {
+      .then((promises) => {
         res.status(200).send({
           rows: promises[0],
-          pages: promises[1]
-        })
+          pages: promises[1],
+        });
       })
-      .catch(error => {
-        res.status(400).send(error)
-      })
+      .catch((error) => {
+        res.status(400).send(error);
+      });
   },
 
   create(req, res) {
@@ -30,31 +30,31 @@ export default {
 
     Users.create({
       res,
-      body: req.body
-    })
+      body: req.body,
+    });
   },
 
   find(req, res) {
     Users.find({
       res,
       where: {
-        userId: req.params.userId
-      }
-    })
+        userId: req.params.userId,
+      },
+    });
   },
 
   update(req, res) {
     Users.update({
       res,
       body: req.body,
-      userId: req.params.userId
-    })
+      userId: req.params.userId,
+    });
   },
 
   destroy(req, res) {
     Users.destroy({
       res,
-      userId: req.params.userId
-    })
-  }
-}
+      userId: req.params.userId,
+    });
+  },
+};
