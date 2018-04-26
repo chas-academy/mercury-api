87f9128b5cfb6ace3322e1dc4147a773742fd0d9
Ludvig Lundgren <hello@ludviglundgren.se>
2018-04-26 23:39:44 +0200

    Update welcome message

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import C from '../controllers'
 
 export default (app) => {
   app.get('/', (req, res) => res.status(200).send({
-    message: 'Hello from 16-test_dependencies! I will be updated to include the dependencies required for testing :)'
+    message: 'Hello from new docker swarm!! Feels guuud!'
   }))
 
   /* Sessions */
