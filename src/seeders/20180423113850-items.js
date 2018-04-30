a977efd416cbe6f430029c6abbbda23fd70d983d
ulander-a <alve.ulander@chasacademy.se>
2018-04-30 13:53:43 +0200

    add user meta seed

--- src/seeders/20180423113850-items.js
+++ src/seeders/20180423113850-items.js
@@ -2,52 +2,52 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-  //   return queryInterface.bulkInsert('Items', [{
-  //     goalDelimiter: 42,
-  //     goalType: 'n/a',
-  //     auto: false,
-  //     createdAt: new Date().toLocaleString(),
-  //     updatedAt: new Date().toLocaleString()
-  //   },
-  //   {
-  //     goalDelimiter: 42,
-  //     goalType: 'n/a',
-  //     auto: false,
-  //     createdAt: new Date().toLocaleString(),
-  //     updatedAt: new Date().toLocaleString()
-  //   },
-  //   {
-  //     goalDelimiter: 42,
-  //     goalType: 'n/a',
-  //     auto: false,
-  //     createdAt: new Date().toLocaleString(),
-  //     updatedAt: new Date().toLocaleString()
-  //   },
-  //   {
-  //     goalDelimiter: 42,
-  //     goalType: 'n/a',
-  //     auto: false,
-  //     createdAt: new Date().toLocaleString(),
-  //     updatedAt: new Date().toLocaleString()
-  //   },
-  //   {
-  //     goalDelimiter: 42,
-  //     goalType: 'n/a',
-  //     auto: false,
-  //     createdAt: new Date().toLocaleString(),
-  //     updatedAt: new Date().toLocaleString()
-  //   }
-  // ])
-  return Promise.all([
-    associateWithCanonical(queryInterface, 42, 'n/a', false, false, 1),
-    associateWithCanonical(queryInterface, 42, 'n/a', false, false, 1),
-    associateWithCanonical(queryInterface, 42, 'n/a', false, false, 2),
-    associateWithCanonical(queryInterface, 42, 'n/a', false, false, 2),
-    associateWithCanonical(queryInterface, 42, 'n/a', false, false, 2)
-  ])
-  .then(promises => {
-    return queryInterface.bulkInsert('Items', promises, {})
-  })
+    //   return queryInterface.bulkInsert('Items', [{
+    //     goalDelimiter: 42,
+    //     goalType: 'n/a',
+    //     auto: false,
+    //     createdAt: new Date().toLocaleString(),
+    //     updatedAt: new Date().toLocaleString()
+    //   },
+    //   {
+    //     goalDelimiter: 42,
+    //     goalType: 'n/a',
+    //     auto: false,
+    //     createdAt: new Date().toLocaleString(),
+    //     updatedAt: new Date().toLocaleString()
+    //   },
+    //   {
+    //     goalDelimiter: 42,
+    //     goalType: 'n/a',
+    //     auto: false,
+    //     createdAt: new Date().toLocaleString(),
+    //     updatedAt: new Date().toLocaleString()
+    //   },
+    //   {
+    //     goalDelimiter: 42,
+    //     goalType: 'n/a',
+    //     auto: false,
+    //     createdAt: new Date().toLocaleString(),
+    //     updatedAt: new Date().toLocaleString()
+    //   },
+    //   {
+    //     goalDelimiter: 42,
+    //     goalType: 'n/a',
+    //     auto: false,
+    //     createdAt: new Date().toLocaleString(),
+    //     updatedAt: new Date().toLocaleString()
+    //   }
+    // ])
+    return Promise.all([
+      associateWithCanonical(queryInterface, 42, 'n/a', false, false, 1),
+      associateWithCanonical(queryInterface, 42, 'n/a', false, false, 1),
+      associateWithCanonical(queryInterface, 42, 'n/a', false, false, 2),
+      associateWithCanonical(queryInterface, 42, 'n/a', false, false, 2),
+      associateWithCanonical(queryInterface, 42, 'n/a', false, false, 2)
+    ])
+      .then(promises => {
+        return queryInterface.bulkInsert('Items', promises, {})
+      })
   },
 
   down: (queryInterface, Sequelize) => {
@@ -56,24 +56,31 @@ module.exports = {
 
 };
 
-function associateWithCanonical(queryInterface, delimiter, goalType, auto, completed, canonicalId) {
+function associateWithCanonical(
+  queryInterface,
+  delimiter,
+  goalType,
+  auto,
+  completed,
+  canonicalId
+) {
   return queryInterface
-  .rawSelect('CanonicalItems', {
-    where: { canonicalId }
-  }, ['canonicalId'])
-  .then(canonicalId => {
-    const date = new Date()
+    .rawSelect('CanonicalItems', {
+      where: { canonicalId }
+    }, ['canonicalId'])
+    .then(canonicalId => {
+      const date = new Date()
 
-    console.log('[Associating Items]', canonicalId)
+      console.log('[Associating Items]', canonicalId)
 
-    return {
-      delimiter,
-      goalType,
-      auto,
-      completed,
-      canonicalId,
-      createdAt: date,
-      updatedAt: date
-    }
-  })
+      return {
+        delimiter,
+        goalType,
+        auto,
+        completed,
+        canonicalId,
+        createdAt: date,
+        updatedAt: date
+      }
+    })
 }
