a1d2edd2eef06eb2432350f4402494615a07cf98
Ludvig Lundgren <hello@ludviglundgren.se>
2018-05-01 18:37:39 +0200

    Update sequlize to use production config. Update dockerfile and env

--- src/config/database.js
+++ src/config/database.js
@@ -18,5 +18,14 @@ module.exports = {
     host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
     migrationStorageTableName: 'sequelize_meta'
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    dialect: 'postgres',
+    migrationStorageTableName: 'sequelize_meta'
   }
+
 }
