f6c4d3a8abce9bffb8d20f997def1c59c5bd52b7
ulander-a <alve.ulander@chasacademy.se>
2018-05-02 10:14:59 +0200

    add user meta seeding

--- src/migrations/20180430095708-create-user-meta.js
+++ src/migrations/20180430095708-create-user-meta.js
@@ -1,28 +1,31 @@
 'use strict';
-export function up(queryInterface, Sequelize) {
-  return queryInterface.createTable('UserMeta', {
-    userMetaId: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: Sequelize.INTEGER
-    },
-    age: {
-      type: Sequelize.INTEGER
-    },
-    location: {
-      type: Sequelize.STRING
-    },
-    createdAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: Sequelize.DATE
-    }
-  });
-}
-export function down(queryInterface, Sequelize) {
-  return queryInterface.dropTable('UserMeta');
-}
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('UserMeta', {
+      userMetaId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      age: {
+        type: Sequelize.INTEGER
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Items');
+  }
+};
