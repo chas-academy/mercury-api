869ba71a02e104807e156a5347df06e493c23125
Ludvig Lundgren <hello@ludviglundgren.se>
2018-05-14 19:58:47 +0200

    Add admin role check to auth for algolia indexing

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,12 +1,11 @@
-import Sessions from './Sessions';
-import Tests from './Tests';
-import Users from './Users';
-import CanonicalItems from './CanonicalItems';
-import Items from './Items';
-import Assoc from './Assoc';
-import UserMeta from './UserMeta';
-import AlgoliaSync from './AlgoliaSync';
-
+import Sessions from "./Sessions";
+import Tests from "./Tests";
+import Users from "./Users";
+import CanonicalItems from "./CanonicalItems";
+import Items from "./Items";
+import Assoc from "./Assoc";
+import UserMeta from "./UserMeta";
+import AlgoliaSync from "./AlgoliaSync";
 
 export default {
   Sessions,
@@ -16,5 +15,5 @@ export default {
   Items,
   Assoc,
   UserMeta,
-  AlgoliaSync,
+  AlgoliaSync
 };
--- src/lib/Sessions.js
+++ src/lib/Sessions.js
@@ -1,23 +1,25 @@
-import JWT from 'jsonwebtoken';
-import Passport from 'passport';
-import HttpBearerStrategy from 'passport-http-bearer';
-import DB from '../models';
-import * as Users from './Users';
+import JWT from "jsonwebtoken";
+import Passport from "passport";
+import HttpBearerStrategy from "passport-http-bearer";
+import DB from "../models";
+import * as Users from "./Users";
 
-Passport.use(new HttpBearerStrategy((token, done) => {
-  const { userId } = verifyToken(null, { token, returnData: true });
+Passport.use(
+  new HttpBearerStrategy((token, done) => {
+    const { userId } = verifyToken(null, { token, returnData: true });
 
-  return find(null, {
-    where: { userId, token },
-    returnData: true,
-  }).then((Session) => {
-    let result = Session;
+    return find(null, {
+      where: { userId, token },
+      returnData: true
+    }).then(Session => {
+      let result = Session;
 
-    if (!Session || (Session && Session.signedOut)) result = false;
+      if (!Session || (Session && Session.signedOut)) result = false;
 
-    return done(null, result);
-  });
-}));
+      return done(null, result);
+    });
+  })
+);
 
 export function find(res, options) {
   const { where, returnData } = options;
@@ -27,12 +29,20 @@ export function find(res, options) {
     include: [
       {
         model: DB.User,
-        as: 'User',
-        attributes: ['userId', 'firstName', 'lastName', 'email', 'role', 'status', 'redirect'],
-      },
-    ],
+        as: "User",
+        attributes: [
+          "userId",
+          "firstName",
+          "lastName",
+          "email",
+          "role",
+          "status",
+          "redirect"
+        ]
+      }
+    ]
   })
-    .then((Session) => {
+    .then(Session => {
       if (returnData) return Session;
 
       return res.status(Session ? 200 : 404).send(Session);
@@ -43,54 +53,96 @@ export function find(res, options) {
 export function auth(req, res) {
   const status = 200;
   let data = {};
-  const { email, password } = verifyToken(res, { token: req.body.token, returnData: true });
+  const { email, password } = verifyToken(res, {
+    token: req.body.token,
+    returnData: true
+  });
   const authResponse = {
     invalid: {
       status: 404,
-      data: { message: "The email or password you entered doesn't match any account." },
+      data: {
+        message: "The email or password you entered doesn't match any account."
+      }
     },
     blocked: {
       status: 401,
-      data: { message: 'Your account is blocked. Please contact the administrator.' },
-    },
+      data: {
+        message: "Your account is blocked. Please contact the administrator."
+      }
+    }
   };
 
-  return Users.find({ where: { email }, returnData: true }).then((User) => {
+  return Users.find({ where: { email }, returnData: true }).then(User => {
     if (!User.object || !(User.object && User.object.authenticate(password))) {
       return authResponse.invalid;
     }
 
-    if (User.json.status === 'blocked') {
+    if (User.json.status === "blocked") {
       return authResponse.blocked;
     }
 
     const date = new Date();
-    const token = JWT.sign(Object.assign({}, User.json, { date }), process.env.JWT_SECRET, {
-      expiresIn: 86400,
-    });
+    const token = JWT.sign(
+      Object.assign({}, User.json, { date }),
+      process.env.JWT_SECRET,
+      {
+        expiresIn: 86400
+      }
+    );
     const sessionData = {
       userId: User.json.userId,
-      userAgent: req.headers['user-agent'],
+      userAgent: req.headers["user-agent"],
       ipAddress: getIpAddress(req),
-      token,
+      token
     };
 
     return DB.Session.create(sessionData).then(() => {
-      data = Object.assign({}, { token }, data, { redirect: User.json.redirect });
+      data = Object.assign({}, { token }, data, {
+        redirect: User.json.redirect
+      });
       return { status, data };
     });
   });
 }
 
 export function signOut(req) {
-  const token = req.headers.authorization.split(' ')[1];
+  const token = req.headers.authorization.split(" ")[1];
   const updatedAt = new Date();
 
-  return DB.Session.update({ signedOut: true, updatedAt }, { where: { token } }).then(() => true);
+  return DB.Session.update(
+    { signedOut: true, updatedAt },
+    { where: { token } }
+  ).then(() => true);
 }
 
 export function authBearer() {
-  return Passport.authenticate('bearer', { session: false });
+  return Passport.authenticate("bearer", { session: false });
+}
+
+export function authAdmin(req, res, next) {
+  const token = req.headers.authorization.split(" ")[1];
+
+  return DB.Session.findOne({
+    where: { token }
+  })
+    .then(Session => {
+      const { userId } = Session;
+
+      return Users.find({ where: { userId }, returnData: true })
+        .then(User => {
+          const userRole = User.json.role;
+
+          if (userRole === "Admin") {
+            return next();
+          } else {
+            return res.status(401).json({
+              message: "Unauthorized access!"
+            });
+          }
+        })
+        .catch(error => console.error(error));
+    })
+    .catch(error => console.error(error));
 }
 
 function verifyToken(res, { token, returnData }) {
@@ -103,9 +155,9 @@ function verifyToken(res, { token, returnData }) {
 
 function getIpAddress(req) {
   return (
-    req.headers['x-forwarded-for'] ||
+    req.headers["x-forwarded-for"] ||
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
     req.connection.socket.remoteAddress
-  ).split(',')[0];
+  ).split(",")[0];
 }
